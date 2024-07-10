a2ec4f90b46d25fc5e8eadf79b8de239af2d5a8f
Maurits Kok <m.w.a.kok@tudelft.nl>
2024-07-10 20:31:25 +0200

    Update inputValidators.m with improved input validation functions

--- src/inputValidators.m
+++ src/inputValidators.m
@@ -2,12 +2,10 @@ function schema = inputValidators()
 % Definition of the data schema 
 %
 % Each parameter will be validated through a defined function handle.
-% Three types of function handles are recommended:
-%   1) Data type identification
-%   (https://nl.mathworks.com/help/matlab/data-type-identification.html)
-%   2) Validate attributes
+% Two types of function handles are supported:
+%   1) Validate attributes
 %   (https://nl.mathworks.com/help/matlab/ref/validateattributes.html)
-%   3) Custom validation function
+%   2) Custom validation function that throws an error if false
 %
 % Returns:
 %   struct : data schema
@@ -15,61 +13,65 @@ function schema = inputValidators()
 schema = struct();
 
 % File details
-schema.name = @ischar;
+schema.name = @(x) validateattributes(x, {'char'}, {'nonempty'});
 
 % Model Settings
-schema.numDeltaLelems = @isnumeric;
-schema.FgToggle = @islogical;
-schema.vertWindProfile = @islogical;
+schema.numDeltaLelems = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.FgToggle = @(x) validateattributes(x, {'logical'}, {'nonempty'});
+schema.vertWindProfile = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
 
 % Wind Parameters
 schema.vw_ref = @(x) validateattributes(x, {'numeric'}, {'increasing', 'positive'});
-schema.h_ref = @isnumeric;
+schema.h_ref = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
 schema.windShearExp = @mustBeValidWindShearExp;
 
 % Sample vertical wind profile datasets
 schema.h_windDataset = @(x) validateattributes(x, {'numeric'}, {'increasing', 'positive'});
-schema.v_windDataset_Cabauw = @isnumeric;
-schema.v_windDataset_Ijmuiden = @isnumeric;
+schema.v_windDataset_Cabauw = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.v_windDataset_Ijmuiden = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+
+% Wind Profile selection
+schema.windProfile_h = @(x) validateattributes(x, {'char'}, {'nonempty'});
+schema.windProfile_vw = @(x) validateattributes(x, {'char'}, {'nonempty'});
 
 % System Parameters
-schema.S = @isnumeric;
-schema.AR = @isnumeric;
-schema.P_ratedElec = @isnumeric;
-schema.massOverride = @isnumeric;
-schema.kiteMass = @isnumeric;
-schema.peakM2E_F = @isnumeric;
+schema.S = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.AR = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.P_ratedElec = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.massOverride = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.kiteMass = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.peakM2E_F =@(x) validateattributes(x, {'numeric'}, {'nonempty'});
 
 % Tether Parameters
-schema.Ft_max = @isnumeric;
-schema.Ft_max_SF = @isnumeric;
-schema.maxTeLen = @isnumeric;
-schema.maxHeight = @isnumeric;
-schema.minGroundClear = @isnumeric;
-schema.Te_matStrength = @isnumeric;
-schema.Te_matDensity = @isnumeric;
+schema.Ft_max = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.Ft_max_SF = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.maxTeLen = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.maxHeight = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.minGroundClear = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.Te_matStrength = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.Te_matDensity = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
 
 % Aerodynamic Parameters
-schema.Cl_maxAirfoil = @isnumeric;
-schema.Cl_eff_F = @isnumeric;
-schema.Cl0_airfoil = @isnumeric;
-schema.e = @isnumeric;
-schema.Cd0 = @isnumeric;
-schema.Cd_c = @isnumeric;
+schema.Cl_maxAirfoil = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.Cl_eff_F = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.Cl0_airfoil = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.e = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.Cd0 = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.Cd_c = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
 
 % Drum Operational Limits
-schema.v_d_max = @isnumeric;
-schema.a_d_max = @isnumeric;
+schema.v_d_max = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.a_d_max = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
 
 % Drivetrain Efficiency Parameters
 schema.etaGen.param = @(x) validateattributes(x, {'numeric'}, {'row', 'numel', 4});
-schema.etaGearbox = @isnumeric;
-schema.etaSto = @isnumeric;
-schema.etaPE = @isnumeric;
+schema.etaGearbox = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.etaSto = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.etaPE = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
 
 % Environmental Constants
-schema.gravity = @isnumeric;
-schema.airDensity = @isnumeric;
+schema.gravity = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
+schema.airDensity = @(x) validateattributes(x, {'numeric'}, {'nonempty'});
 
 end
 
