1e3db5eb2708a82f4094dc41c9701880c184f548
Maurits Kok <m.w.a.kok@tudelft.nl>
2024-07-10 21:26:49 +0200

    Remove data transfer from yaml to inputs

--- src/loadInputs.m
+++ src/loadInputs.m
@@ -1,82 +1,47 @@
-function inputs = loadInputs(yamlFilePath)
-    % Check if the file exists
-    if exist(yamlFilePath, 'file') ~= 2
-        error('File not found: %s', yamlFilePath);
-    end
-    
-    % Read the YAML file
-    yamlData = yaml.ReadYaml(yamlFilePath);
+function inputs = loadInputs(filepath)
+% Load inputs from .yml and .m files 
+%
+% Args:
+%   filepath (char): path to the parameters file
+%
+% Returns:
+%   struct
 
-    % Get all field names
-    fields = fieldnames(yamlData);
-    
-    % Loop through each field and convert cell arrays to double arrays
-    for i = 1:numel(fields)
-        field = fields{i};
-        if iscell(yamlData.(field))
-            yamlData.(field) = cell2mat(yamlData.(field));
-        end
-    end
-    
-    % Populate the inputs structure
-    inputs = struct();
-    
-    % Define Input Structure
-    inputs.name = yamlData.name;
-    
-    % Model Settings
-    inputs.numDeltaLelems = yamlData.numDeltaLelems;
-    inputs.FgToggle = yamlData.FgToggle;
-    inputs.vertWindProfile = yamlData.vertWindProfile;
-    
-    % Wind Parameters
-    inputs.vw_ref = yamlData.vw_ref;
-    inputs.h_ref = yamlData.h_ref;
-    inputs.windShearExp = yamlData.windShearExp;
-    
-    % Sample vertical wind profile datasets (for inputs.vertWindProfile = 1)
-    inputs.h_windDataset = yamlData.h_windDataset;
-    inputs.v_windDataset_Cabauw = yamlData.v_windDataset_Cabauw;
-    inputs.v_windDataset_Ijmuiden = yamlData.v_windDataset_Ijmuiden;
-    inputs.windProfile_h = yamlData.h_windDataset;
-    inputs.windProfile_vw = yamlData.v_windDataset_Cabauw;
-    
-    % System Parameters
-    inputs.S = yamlData.S;
-    inputs.AR = yamlData.AR;
-    inputs.P_ratedElec = yamlData.P_ratedElec;
-    inputs.massOverride = yamlData.massOverride;
-    inputs.kiteMass = yamlData.kiteMass;
-    inputs.peakM2E_F = yamlData.peakM2E_F;
-    
-    % Tether Parameters
-    inputs.Ft_max = yamlData.Ft_max;
-    inputs.Ft_max_SF = yamlData.Ft_max_SF;
-    inputs.maxTeLen = yamlData.maxTeLen;
-    inputs.maxHeight = yamlData.maxHeight;
-    inputs.minGroundClear = yamlData.minGroundClear;
-    inputs.Te_matStrength = yamlData.Te_matStrength;
-    inputs.Te_matDensity = yamlData.Te_matDensity;
-    
-    % Aerodynamic Parameters
-    inputs.Cl_maxAirfoil = yamlData.Cl_maxAirfoil;
-    inputs.Cl_eff_F = yamlData.Cl_eff_F;
-    inputs.Cl0_airfoil = yamlData.Cl0_airfoil;
-    inputs.e = yamlData.e;
-    inputs.Cd0 = yamlData.Cd0;
-    inputs.Cd_c = yamlData.Cd_c;
-    
-    % Drum operational Limits
-    inputs.v_d_max = yamlData.v_d_max;
-    inputs.a_d_max = yamlData.a_d_max;
-    
-    % Drivetrain Efficiency Parameters
-    inputs.etaGen.param = yamlData.etaGen_param;
-    inputs.etaGearbox = yamlData.etaGearbox;
-    inputs.etaSto = yamlData.etaSto;
-    inputs.etaPE = yamlData.etaPE;
-    
-    % Environmental Constants
-    inputs.gravity = yamlData.gravity;
-    inputs.airDensity = yamlData.airDensity;
+% Check if the file exists
+if exist(filepath, 'file') ~= 2
+    error('File not found: %s', filepath);
 end
+
+[~, name, ext] = fileparts(filepath);
+
+switch ext
+    case {'.yml', '.yaml'}        
+        inputs = yaml.ReadYaml(filepath);        
+    
+        % Get all field names
+        fields = fieldnames(inputs);
+        
+        % Loop through each field and convert cell arrays to double arrays
+        % TODO: replace with recursive function
+        for i = 1:numel(fields)
+            field = fields{i};
+            if isstruct(inputs.(field))
+                subfields = fieldnames(inputs.(field));
+                for j = 1:numel(subfields)
+                    subfield = subfields{j};
+                    if iscell(inputs.(field).(subfield))
+                        inputs.(field).(subfield) = cell2mat(inputs.(field).(subfield));
+                    end
+                end
+            elseif iscell(inputs.(field))
+                inputs.(field) = cell2mat(inputs.(field));
+            end
+        end
+
+    case '.m'
+        inputs = feval(name);
+
+    otherwise
+        error('Expected input file of type .yml or .m, instead received %s', ext);
+end
+
